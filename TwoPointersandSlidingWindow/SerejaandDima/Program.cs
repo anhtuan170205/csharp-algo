39981f5a535b41cfafc96a6319e1e66a547ea860
anhtuan170205 <anhtuantran170205@gmail.com>
2026-07-08 23:37:48 +0700

    Sereja and Dima

---
+++ TwoPointersandSlidingWindow/SerejaandDima/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+public class Program
+{
+	public static void Main()
+	{
+		int n = int.Parse(Console.ReadLine());
+		
+		int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+		
+		int sSum = 0, dSum = 0, turn = 0;
+		int left = 0, right = n - 1;
+		
+		while (left <= right)
+		{
+			int answer;
+			if (a[left] >= a[right])
+			{
+				answer = a[left];
+				left++;
+			}
+			else
+			{
+				answer = a[right];
+				right--;
+			}
+		
+			if (turn % 2 == 0)
+			{
+				sSum += answer;
+			}
+			else
+			{
+				dSum += answer;
+			}
+				turn++;
+			}
+			
+		Console.WriteLine(sSum + " " + dSum);
+	}
+}
