9a6c4a9ec40042a106e9880503a6dc7dbcb99645
anhtuan170205 <anhtuantran170205@gmail.com>
2026-08-08 21:45:56 +0700

    George and Round

---
+++ TPandSW/GeorgeandRound/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+public class Program
+{
+	public static void Main()
+  {
+  	int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    int n = input[0], m = input[1];
+    
+    int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    int[] b = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    
+    int aidx = 0, bidx = 0;
+    
+    while (aidx < n && bidx < m)
+    {
+    	if (a[aidx] <= b[bidx])
+      {
+      	aidx++;
+      }
+			bidx++;
+		}
+        
+    int count = n - aidx;
+    
+    Console.WriteLine(count);
+	}
+}
